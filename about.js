e1d74a01f885faa248b3c3a1e3b098f8896b59f5
William Cuthbertson <whc1315@gmail.com>
2022-01-21 08:41:14 -0700

    part 2 about.js done

--- about.js
+++ about.js
@@ -1,14 +1,20 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+  evt.preventDefault();
+
+  alert("Form has been submitted!");
+  // console.log('form submit');
 }
 
+let form = document.querySelector("form#contact");
 
-let form = document.querySelector('form#contact');
+form.addEventListener("submit", handleSubmit);
 
-form.addEventListener('submit', handleSubmit);
+function catCompliment() {
+  alert("Your hair looks nice!");
+}
+
+let pic = document.querySelector("img#cat");
+
+pic.addEventListener("mouseover", catCompliment);
